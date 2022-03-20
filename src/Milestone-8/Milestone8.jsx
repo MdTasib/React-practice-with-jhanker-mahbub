c6bc69a0332ddc4c9d3de6e38337eb04d16c5859
MdTasib <ohidulalam442@gmail.com>
2022-03-20 22:21:58 +0600

    storage data handler

--- src/Milestone-8/Milestone8.jsx
+++ src/Milestone-8/Milestone8.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Core_Concept from "./Core_Concept/Core_Concept";
 import Recap_Core_Concepet from "./Core_Concept/Recap_Core_Concept";
+import Storage_Data_Handler from "./Storage_Data_Handler/Storage_Data_Handler";
 
 const Milestone8 = () => {
 	return (
 		<div>
 			{/* <Core_Concept /> */}
 			{/* <Recap_Core_Concepet /> */}
+			<Storage_Data_Handler/>
 		</div>
 	)
 };
---
+++ src/Milestone-8/Storage_Data_Handler/Storage_Data_Handler.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Storage_Data_Handler = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Storage_Data_Handler;
