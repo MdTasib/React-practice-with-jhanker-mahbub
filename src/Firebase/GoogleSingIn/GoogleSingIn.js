b5f2ac9b58916112772b12b032a0afa432c5a88a
MdTasib <ohidulalam442@gmail.com>
2022-04-06 16:03:11 +0600

    sing out

--- src/Firebase/GoogleSingIn/GoogleSingIn.js
+++ src/Firebase/GoogleSingIn/GoogleSingIn.js
@@ -1,25 +1,47 @@
-import React from "react";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import React, { useState } from "react";
+import {
+	getAuth,
+	GoogleAuthProvider,
+	signInWithPopup,
+	signOut,
+} from "firebase/auth";
 import app from "../firebase.init";
 
 const auth = getAuth(app);
 
 const GoogleSingIn = () => {
+	const [user, setUser] = useState({});
+
 	const provider = new GoogleAuthProvider();
-	const handleWithGoogle = () => {
+	const handleSingIn = () => {
 		signInWithPopup(auth, provider)
 			.then(result => {
 				const user = result.user;
-				console.log(user);
+				setUser(user);
 			})
 			.catch(error => {
 				console.log(error);
 			});
 	};
 
+	const handleSingOut = () => {
+		signOut(auth)
+			.then(() => {
+				setUser({});
+			})
+			.catch(error => console.log(error));
+	};
+
 	return (
 		<div>
-			<button onClick={handleWithGoogle}>Google Log In</button>
+			{user.email ? (
+				<button onClick={handleSingOut}>Sing Out</button>
+			) : (
+				<button onClick={handleSingIn}>Google Log In</button>
+			)}
+			<h3>{user.displayName}</h3>
+			<p>{user.email}</p>
+			<img src={user.photoURL} alt='' />
 		</div>
 	);
 };
