768dc9d48d14d3ebd0c614305f29b34677b9ac45
MdTasib <ohidulalam442@gmail.com>
2022-04-07 12:00:42 +0600

    form validation

--- src/Firebase/EmailAndPassword/EmailAndPassword.js
+++ src/Firebase/EmailAndPassword/EmailAndPassword.js
@@ -1,35 +1,56 @@
-import React from "react";
-import { getAuth } from "firebase/auth";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from "../firebase.init";
 import { Button, Form } from "react-bootstrap";
 
 const auth = getAuth(app);
 
 const EmailAndPassword = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [validated, setValidated] = useState(false);
+
 	const handleEmail = event => {
-		console.log(event.target.value);
+		setEmail(event.target.value);
 	};
 
 	const handlePassword = event => {
-		console.log(event.target.value);
+		setPassword(event.target.value);
 	};
 
 	const handleSubmit = event => {
 		event.preventDefault();
-		console.log("form submited");
+		const form = event.currentTarget;
+		if (form.checkValidity() === false) {
+			event.stopPropagation();
+			return;
+		}
+
+		setValidated(true);
+
+		createUserWithEmailAndPassword(auth, email, password)
+			.then(result => {
+				const user = result.user;
+				console.log(user);
+			})
+			.catch(error => console.error(error));
 	};
 
 	return (
 		<div className='w-50 mx-auto mt-5'>
 			<h3>User Email And Password</h3>
-			<Form onSubmit={handleSubmit}>
+			<Form noValidate validated={validated} onSubmit={handleSubmit}>
 				<Form.Group className='mb-3' controlId='formBasicEmail'>
 					<Form.Label>Email address</Form.Label>
 					<Form.Control
 						onBlur={handleEmail}
 						type='email'
 						placeholder='Enter email'
+						required
 					/>
+					<Form.Control.Feedback type='invalid'>
+						Please provide a valid Email.
+					</Form.Control.Feedback>
 				</Form.Group>
 
 				<Form.Group className='mb-3' controlId='formBasicPassword'>
@@ -38,7 +59,11 @@ const EmailAndPassword = () => {
 						onBlur={handlePassword}
 						type='password'
 						placeholder='Password'
+						required
 					/>
+					<Form.Control.Feedback type='invalid'>
+						Please provide a valid Password.
+					</Form.Control.Feedback>
 				</Form.Group>
 				<Button variant='primary' type='submit'>
 					Submit
