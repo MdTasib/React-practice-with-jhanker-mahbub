9be8dad829d15d3060376c4c63b9bcfa0ebda9ac
MdTasib <ohidulalam442@gmail.com>
2022-04-09 13:56:45 +0600

    RequireAuth || PrivateRoute

--- src/Firebase/PrivateRoute/PrivateRoute.js
+++ src/Firebase/PrivateRoute/PrivateRoute.js
@@ -3,7 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
+import Orders from "./components/Orders/Orders";
 import Register from "./components/Register/Register";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
 
 const PrivateRoute = () => {
 	return (
@@ -14,6 +16,14 @@ const PrivateRoute = () => {
 				<Route path='/' element={<Home />} />
 				<Route path='/login' element={<Login />} />
 				<Route path='/register' element={<Register />} />
+				<Route
+					path='/orders'
+					element={
+						<RequireAuth>
+							<Orders />
+						</RequireAuth>
+					}
+				/>
 			</Routes>
 		</div>
 	);
---
+++ src/Firebase/PrivateRoute/components/RequireAuth/RequireAuth.js
@@ -0,0 +1,15 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useFirebase from "../../hooks/useFirebase";
+
+const RequireAuth = ({ children }) => {
+	const { user } = useFirebase();
+	const location = useLocation();
+
+	if (!user) {
+		return <Navigate to='/login' state={{ from: location }} replace />;
+	}
+
+	return children;
+};
+
+export default RequireAuth;
